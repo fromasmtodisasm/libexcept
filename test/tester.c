086be9b88f7d35c0295abd766b2a31c94c098808
Simon Howard <fraggle@gmail.com>
2006-09-28 19:35:15 +0000

    Newer version of test program.

--- test/tester.c
+++ test/tester.c
@@ -2,32 +2,43 @@
 #include <stdio.h>
 #include "libexcept.h"
 
+ExceptionType ShoeException = EXCEPTION_TYPE(BaseException);
+ExceptionType TimberlandException = EXCEPTION_TYPE(ShoeException);
+ExceptionType MonkeyException = EXCEPTION_TYPE(BaseException);
+
 void myfunc(void)
 {
-        EXCEPT_TRY
-        {
-                except_raise("Error!");
-        }
-        EXCEPT_FINALLY
-        {
-                printf("In finally block\n");
-        }
-        EXCEPT_END
+    except_try {
+        except_raise(TimberlandException, "Error!");
+    } except_finally {
+        printf("In finally block\n");
+    } except_end
 }
 
 int main(int argc, char *argv[])
 {
-        EXCEPT_TRY
-        {
-                myfunc();
-        }
-/*        EXCEPT_CATCH(exception_t *exception)
-        {
-                printf("Caught an exception!\n");
-                exception_free(exception);
-        }*/
-        EXCEPT_END
+    int i;
 
-        return 0;
+    except_try {
+        for (i=0; i<10; ++i) {
+            myfunc();
+        }
+    } except_catch(MonkeyException, exception) {
+        printf("Caught a MonkeyException!\n");
+    } except_catch(ShoeException, exception) {
+        printf("Caught a ShoeException!\n");
+    } except_finally {
+        printf("main finally\n");
+    } except_end
+
+    except_try {
+        except_assert(1 == 0);
+    } except_catch(ShoeException, exception) {
+        printf("Caught a ShoeException!\n");
+    } except_catch(AssertException, exception) {
+        printf("Caught an assert failure\n");
+    } except_end
+
+    return 0;
 }
 
