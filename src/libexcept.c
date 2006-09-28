e51fa1342ffc036a38964b1391ecca5fcaa3d127
Simon Howard <fraggle@gmail.com>
2006-09-28 19:29:11 +0000

    More up to date version - add exception hierarchies, change structure names to Gtk-style, guard against use of return/goto/break etc.

--- src/libexcept.c
+++ src/libexcept.c
@@ -4,35 +4,137 @@
 
 #include "libexcept.h"
 
-struct exception_s
-{
+/* Work out how to get thread-local storage. */
+
+#if defined(__GNUC__)
+
+#define TLS_PREFIX __thread
+
+#elif defined(_MSC_VER)
+
+#define TLS_PREFIX __declspec(thread)
+
+#else
+
+#warning Unknown platform - don't know how to use thread-local storage.  \
+         exceptions in multithreaded programs may break.
+#define TLS_PREFIX
+
+#endif
+
+typedef enum {
+        STACK_UNKNOWN, 
+        STACK_DOWNWARD,       /* Stack grows downward, ie. toward 0 */
+        STACK_UPWARD,         /* Stack grows upward, ie. away from 0 */
+} stack_direction_t;
+
+struct _Exception {
+    ExceptionType *type;
     void *user_data;
     char *file;
-    char *line;
+    int line;
 };
 
-static except_data_t *except_chain_head = NULL;
-static exception_t *current_exception = NULL;
+/* Base exception type that all inherit from */
 
-void __except_add(except_data_t *data)
+ExceptionType BaseException = { NULL };
+
+/* Exception type thrown by except_assert() */
+
+ExceptionType AssertException = EXCEPTION_TYPE(BaseException);
+
+static stack_direction_t stack_direction = STACK_UNKNOWN;
+static TLS_PREFIX ExceptData *except_chain_head = NULL;
+static TLS_PREFIX Exception *current_exception = NULL;
+
+static stack_direction_t find_stack_direction_2(int *parent_var)
 {
+        int variable;
+
+        variable = 5;
+
+        if (&variable > parent_var)
+        {
+                return STACK_UPWARD;
+        }
+        else if (&variable < parent_var)
+        {
+                return STACK_DOWNWARD;
+        }
+        else
+        {
+                // This should never happen, except maybe due to some 
+                // strange compiler optimisations.
+
+                fprintf(stderr, "Unable to determine stack direction!\n");
+                exit(-1);
+
+                return STACK_UNKNOWN;
+        }
+}
+
+static stack_direction_t find_stack_direction(void)
+{
+        int variable;
+
+        variable = 1;
+
+        return find_stack_direction_2(&variable);
+}
+
+void __except_bug(char *file, int line)
+{
+    fprintf(stderr, "%s:%i: BUG detected: Check that you are not using "
+                    "break, continue, return or goto from within an "
+                    "exception block.\n", file, line);
+
+    exit(-1);
+}
+
+void __except_add(ExceptData *data, char *file, int line)
+{
+    /* Find the direction the stack grows, if we have not done
+     * so already. */
+
+    if (stack_direction == STACK_UNKNOWN)
+    {
+        stack_direction = find_stack_direction();
+    }
+
+    /* Check that objects are being added into the chain in stack order.
+     * If not, there is probably a bug in the calling program. */
+
+    if (except_chain_head != NULL)
+    {
+        if ((stack_direction == STACK_UPWARD 
+             && data <= except_chain_head)
+         || (stack_direction == STACK_DOWNWARD
+             && data >= except_chain_head))
+        {
+            __except_bug(file, line);
+        }
+    }
+    
+    /* Add to the linked list. */
+
     data->next = except_chain_head;
     except_chain_head = data;
 }
 
-void __except_remove(except_data_t *data)
+void __except_remove(ExceptData *data)
 {
-    if (data == except_chain_head)
-    {
+    if (data == except_chain_head) {
         except_chain_head = except_chain_head->next;
     }
 }
 
-exception_t *__exception_new(void *data, char *file, char *line)
+Exception *__exception_new(ExceptionType *type, void *data, 
+                           char *file, int line)
 {
-    exception_t *exception;
+    Exception *exception;
 
-    exception = (exception_t *) malloc(sizeof(exception_t));
+    exception = (Exception *) malloc(sizeof(Exception));
+    exception->type = type;
     exception->user_data = data;
     exception->file = file;
     exception->line = line;
@@ -40,20 +142,59 @@ exception_t *__exception_new(void *data, char *file, char *line)
     return exception;
 }
 
-void exception_free(exception_t *exception)
+ExceptionType *exception_get_type(Exception *exception)
+{
+    return exception->type;
+}
+
+void *exception_get_data(Exception *exception)
+{
+    return exception->user_data;
+}
+
+char *exception_get_file(Exception *exception)
+{
+    return exception->file;
+}
+
+int exception_get_line(Exception *exception)
+{
+    return exception->line;
+}
+
+void __exception_free(Exception *exception)
 {
     free(exception);
 }
 
-void except_throw(exception_t *exception)
+Exception *__except_get_current(void)
+{
+    return current_exception;
+}
+
+int __exception_is_a(Exception *exception, ExceptionType *type)
+{
+    ExceptionType *test_type;
+
+    for (test_type = exception->type; 
+         test_type != NULL; 
+         test_type = test_type->parent) {
+        if (test_type == type) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+void except_throw(Exception *exception)
 {
     /* Jump to the first in the chain */
 
-    if (except_chain_head == NULL)
-    {
+    if (except_chain_head == NULL) {
         /* Nothing is catching this exception.  Bomb out with an error. */
 
-        fprintf(stderr, "ERROR: Uncaught exception at %s, %s\n",
+        fprintf(stderr, "ERROR: Uncaught exception at %s:%i\n",
                 exception->file, exception->line);
         exit(-1);
     }
@@ -63,10 +204,5 @@ void except_throw(exception_t *exception)
     /* Call the first in the chain */
 
     longjmp(except_chain_head->jmp_location, 1);
-}
-
-exception_t *__except_get_current(void)
-{
-        return current_exception;
 }
 
--- src/libexcept.h
+++ src/libexcept.h
@@ -3,58 +3,110 @@
 #define LIBEXCEPT_H
 
 #include <setjmp.h>
+ 
+typedef struct _ExceptionType ExceptionType;
+typedef struct _Exception Exception;
+typedef struct _ExceptData ExceptData;
 
-typedef struct exception_s exception_t;
-typedef struct except_data_s except_data_t;
-
-struct except_data_s
-{
+struct _ExceptData {
     jmp_buf jmp_location;
-    except_data_t *next;
+    ExceptData *next;
 };
 
-#define EXCEPT_TRY                                                      \
-    {                                                                   \
-        except_data_t __except_data;                                    \
-        __except_add(&__except_data);                                   \
-        int __except_throwing = setjmp(__except_data.jmp_location);     \
-        int __except_uncaught = 1;                                      \
-        if (!__except_throwing)                                         \
-        {
+struct _ExceptionType {
+    ExceptionType *parent;
+};
 
-#define EXCEPT_CATCH(var)                                               \
+#define EXCEPTION_TYPE(parent)  { &(parent) }
+
+#define EXCEPT_LOOP_GUARD_START                                         \
+    {                                                                   \
+        int __except_break = 1;                                         \
+        do {
+
+#define EXCEPT_LOOP_GUARD_END                                           \
+            __except_break = 0;                                         \
+        } while (0);                                                    \
+        if (__except_break) {                                           \
+            /* break or continue was used to break out of the loop.     \
+             * This is a BUG in the calling program.  Bomb out with     \
+             * an error. */                                             \
+            __except_bug(__FILE__, __LINE__);                           \
+        }                                                               \
+    }
+
+#define except_try                                                      \
+    {                                                                   \
+        EXCEPT_LOOP_GUARD_START                                         \
+        ExceptData __except_data;                                       \
+        int __except_throwing;                                          \
+        int __except_caught = 0;                                        \
+        Exception *__except_exception = 0;                              \
+        __except_add(&__except_data, __FILE__, __LINE__);               \
+        __except_throwing = setjmp(__except_data.jmp_location);         \
+        if (__except_throwing) {                                        \
+            __except_exception = __except_get_current();                \
+        } else {
+
+#define except_catch(type, var)                                         \
         }                                                               \
         __except_remove(&__except_data);                                \
-        if (__except_throwing)                                          \
-        {                                                               \
-            var = __except_get_current();                               \
-            __except_uncaught = 0;
+        if (__except_throwing && !__except_caught                       \
+         && __exception_is_a(__except_exception, &(type))) {            \
+            Exception *(var);                                           \
+            (var) = __except_exception;                                 \
+            __except_caught = 1;
 
-#define EXCEPT_FINALLY                                                  \
+#define except_finally                                                  \
         }                                                               \
         __except_remove(&__except_data);                                \
         /* always do this */                                            \
         {
             
-#define EXCEPT_END                                                      \
+#define except_end                                                      \
         }                                                               \
         __except_remove(&__except_data);                                \
-        if (__except_throwing && __except_uncaught)                     \
-        {                                                               \
-            /* rethrow the exception */                                 \
-            except_throw(__except_get_current());                       \
+        if (__except_caught) {                                          \
+            /* we caught the exception; now free it */                  \
+            __exception_free(__except_exception);                       \
+        } else if (__except_throwing) {                                 \
+            /* exception was not caught; rethrow it */                  \
+            except_throw(__except_exception);                           \
         }                                                               \
+        EXCEPT_LOOP_GUARD_END                                           \
+    } 
+
+
+#define __except_assert(condition, file, line)                          \
+    if (!(condition)) {                                                 \
+        except_raise(AssertException,                                   \
+                     "Assertation failed: " file #line);                \
     }
 
-void __except_add(except_data_t *data);
-void __except_remove(except_data_t *data);
-exception_t *__except_get_current(void);
+#define except_assert(condition)                                        \
+    __except_assert((condition), __FILE__, __LINE__)
 
-exception_t *__exception_new(void *data, char *file, char *line);
-#define exception_new(data)   __exception_new((data), __FILE__, __LINE__)
-void exception_free(exception_t *exception);
-void except_throw(exception_t *exception);
-#define except_raise(data)    except_throw(exception_new(data))
+void __except_add(ExceptData *data, char *file, int line);
+void __except_remove(ExceptData *data);
+Exception *__except_get_current(void);
+void __exception_free(Exception *exception);
+int __exception_is_a(Exception *exception, ExceptionType *type);
+
+Exception *__exception_new(ExceptionType *type, void *data, 
+                           char *file, int line);
+#define exception_new(type, data)                                       \
+      __exception_new(&(type), (data), __FILE__, __LINE__)
+ExceptionType *exception_get_type(Exception *exception);
+void *exception_get_data(Exception *exception);
+char *exception_get_file(Exception *exception);
+int exception_get_line(Exception *exception);
+
+void except_throw(Exception *exception);
+#define except_raise(type, data)                                        \
+        except_throw(exception_new((type), (data)))
+
+extern ExceptionType BaseException;
+extern ExceptionType AssertException;
 
 #endif /* #ifndef LIBEXCEPT_H */
 
