eecf999c9f01834f5e53ab61c13f1b49112264ad
Simon Howard <fraggle@gmail.com>
2006-10-16 17:37:10 +0000

    Use some more generic exception names in the test program.

--- test/tester.c
+++ test/tester.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 #include "libexcept.h"
 
-ExceptionType ShoeException = EXCEPTION_TYPE(BaseException);
-ExceptionType TimberlandException = EXCEPTION_TYPE(ShoeException);
-ExceptionType MonkeyException = EXCEPTION_TYPE(BaseException);
+ExceptionType OrangeException = EXCEPTION_TYPE(BaseException);
+ExceptionType SatsumaException = EXCEPTION_TYPE(OrangeException);
+ExceptionType AppleException = EXCEPTION_TYPE(BaseException);
 
 void myfunc(void)
 {
     except_try {
-        except_raise(TimberlandException, "Error!");
+        except_raise(SatsumaException, "Error!");
     } except_finally {
         printf("In finally block\n");
     } except_end
@@ -23,18 +23,18 @@ int main(int argc, char *argv[])
         for (i=0; i<10; ++i) {
             myfunc();
         }
-    } except_catch(MonkeyException, exception) {
-        printf("Caught a MonkeyException!\n");
-    } except_catch(ShoeException, exception) {
-        printf("Caught a ShoeException!\n");
+    } except_catch(AppleException, exception) {
+        printf("Caught a AppleException!\n");
+    } except_catch(OrangeException, exception) {
+        printf("Caught a OrangeException!\n");
     } except_finally {
         printf("main finally\n");
     } except_end
 
     except_try {
         except_assert(1 == 0);
-    } except_catch(ShoeException, exception) {
-        printf("Caught a ShoeException!\n");
+    } except_catch(OrangeException, exception) {
+        printf("Caught a OrangeException!\n");
     } except_catch(AssertException, exception) {
         printf("Caught an assert failure\n");
     } except_end
